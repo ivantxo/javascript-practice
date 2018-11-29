c6a542e670681472f62b3af015c02d450d53cea4
Ivan <ivan.ramirez@ladbrokes.com.au>
2018-11-29 17:59:21 +1100

    Object and Array constructing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-//------------ Arrow functions ------------//
+//----------------------------- Arrow functions -----------------------------//
 // ES5
 function arrowFunc(name) {
   return 'Hello ' + name;
@@ -15,7 +15,7 @@ const arrowFuncEs6 = name => {
 
 const arrowFuncEs62 = name => `Hello ${name}`;
 
-//------------ Arrow functions with built-in functions ------------//
+//----------------------------- Arrow functions with built-in functions -----------------------------//
 const builtArray = ['Hau', 'Pradeep', 'Ivan', 5];
 
 // ES5
@@ -26,7 +26,7 @@ let builtArr1 = builtArray.map(function(item) {
 // ES6
 let builtArr2 = builtArray.map(item => item + ', ');
 
-//------------ Template literals ------------//
+//----------------------------- Template literals -----------------------------//
 
 // ES5
 function literals(name, age) {
@@ -36,8 +36,30 @@ function literals(name, age) {
 // ES6
 const literalsES6 = (name, age) => `Hello ${name}. Your age is ${age} years old.`;
 
-//------------ Default parameters ------------//
+//----------------------------- Default parameters -----------------------------//
 const defaultParamsES6 = (name, age = 22) => `Hello ${name}. Your age is ${age} years old`;
+
+//----------------------------- Object and Array constructing -----------------------------//
+
+// ES5
+const contact = {
+  name: 'Ivan',
+  surname: 'Ramirez',
+  age: 26,
+}
+let name = contact.name;
+let surname = contact.surname;
+let age = contact.age;
+
+// ES6
+let {name: name2, surname: surname2, age: age2} = contact;
+
+const players = ['Lionel', 'Roman', 'James', 3];
+let [value1, value2, value3] = players;
+
+
+
+
 
 
 class App extends Component {
@@ -52,6 +74,9 @@ class App extends Component {
         <p>Template Literals ES5: {literals('Ivan', 20)}</p>
         <p>Template Literals ES6: {literalsES6('Sonia', 22)}</p>
         <p>Default parameters ES6: {defaultParamsES6('Sonia')}</p>
+        <p>Structures ES5: {`Contact: ${name}, ${surname}, ${age}`}</p>
+        <p>Structures ES6: {`Contact: ${name2}, ${surname2}, ${age2}`}</p>
+        <p>Arrays ES6: {`Contact: ${value1}, ${value2}, ${value3}`}</p>
         </header>
       </div>
     );
