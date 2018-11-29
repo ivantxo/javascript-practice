f610179d31d5523d5ee3ee3b7ed12df9a6acbde8
Ivan <ivan.ramirez@ladbrokes.com.au>
2018-11-29 16:44:23 +1100

    Arrow functions

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//------------ Arrow functions ------------//
+// ES5
+function arrowFunc(name) {
+  return 'Hello ' + name;
+}
+
+// ES6
+const arrowFuncEs6 = name => {
+  return `Hello ${name}`;
+}
+
+const arrowFuncEs62 = name => `Hello ${name}`;
+
+//------------ Arrow functions with built-in functions ------------//
+const builtArray = ['Hau', 'Pradeep', 'Ivan', 5];
+
+// ES5
+let builtArr1 = builtArray.map(function(item) {
+  return item + ', ';
+});
+
+// ES6
+let builtArr2 = builtArray.map(item => item + ', ');
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <p>Arrow Functions 1: {arrowFuncEs6('Ivan')}</p>
+        <p>Arrow Functions 2: {arrowFuncEs62('Sonia')}</p>
+        <p>Built-in ES5: {builtArr1}</p>
+        <p>Built-in ES6: {builtArr2}</p>
         </header>
       </div>
     );
